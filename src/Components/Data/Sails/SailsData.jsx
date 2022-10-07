8311ad8e7859b1aa8cc5f1f9d1f69248791bec5c
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-07 13:57:35 -0500

    Bregoli

--- src/Components/Data/Sails/SailsData.jsx
+++ src/Components/Data/Sails/SailsData.jsx
@@ -1,7 +1,7 @@
 const SailsData = [
   {
     id: 1,
-    "Header Image": "",
+    HeaderImage: "https://github.com/Dev-Rook/learn-j22/blob/master/src/Assests/Images/Spinnaker.jpeg?raw=true",
     Title: "Sails",
     Description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis consectetur excepturi minima? Doloribus repudiandae minima incidunt harum praesentium aliquam ullam!",
