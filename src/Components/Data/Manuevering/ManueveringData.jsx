d0c1bf28b657ea8b4a532ad744732a1956aaf1fb
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-07 13:59:13 -0500

    Maneuvering Image

--- src/Components/Data/Manuevering/ManueveringData.jsx
+++ src/Components/Data/Manuevering/ManueveringData.jsx
@@ -1,7 +1,7 @@
 const ManueveringData = [
   {
     id: 1,
-    "Header Image": "",
+    HeaderImage: "https://github.com/Dev-Rook/learn-j22/blob/master/src/Assests/Images/Sunny-Upwind.jpg?raw=true",
     Title: "Manuevering",
     Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis consectetur excepturi minima? Doloribus repudiandae minima incidunt harum praesentium aliquam ullam!",
   },
